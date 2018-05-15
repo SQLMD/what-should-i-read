66d03196e3ce54726dbc1ee149abb6f5d08c1253
Mike Donnelly <sqldonnelly@gmail.com>
2018-05-15 22:09:53 +0900

    Separate logic into router and controller

--- index.js
+++ index.js
@@ -1,41 +1,13 @@
 const express = require("express");
-const axios = require("axios");
+
 const app = express();
 app.use(express.static(`${__dirname}/public`));
 app.set("view engine", "ejs");
 
-const API_KEY = process.env.API_KEY;
-let fictionList = true;
+const apiRouter = require("./resources/router");
 
-app.get("/", (req, res) => {
-  let list = "fiction";
-  if (fictionList) {
-    list = "nonfiction";
-  }
-  axios
-    .get("https://api.nytimes.com/svc/books/v3/lists.json", {
-      params: {
-        "api-key": API_KEY,
-        list: "combined-print-and-e-book-" + list
-      }
-    })
-    .then(response => {
-      body = response.data;
-      const randomBook = Math.floor(Math.random() * body.results.length);
-      const title = body.results[
-        randomBook
-      ].book_details[0].title.toLowerCase();
-      const author = body.results[
-        randomBook
-      ].book_details[0].author.toLowerCase();
-      const url = body.results[randomBook].amazon_product_url;
-      fictionList = !fictionList;
-      res.render("main", { author, title, url });
-    })
-    .catch(err => {
-      console.log("Error:", err);
-      res.status(500).send("Server Error!");
-    });
-});
+app.use("/", apiRouter);
 
 app.listen(process.env.PORT || 3000, () => console.log("Server is running"));
+
+module.exports = app;
---
+++ resources/controller.js
@@ -0,0 +1,37 @@
+const axios = require("axios");
+const API_KEY = process.env.API_KEY;
+
+let fictionList = true;
+
+module.exports = {
+  getRandomBook(req, res) {
+    let list = "fiction";
+    if (fictionList) {
+      list = "nonfiction";
+    }
+    axios
+      .get("https://api.nytimes.com/svc/books/v3/lists.json", {
+        params: {
+          "api-key": API_KEY,
+          list: "combined-print-and-e-book-" + list
+        }
+      })
+      .then(response => {
+        body = response.data;
+        const randomBook = Math.floor(Math.random() * body.results.length);
+        const title = body.results[
+          randomBook
+        ].book_details[0].title.toLowerCase();
+        const author = body.results[
+          randomBook
+        ].book_details[0].author.toLowerCase();
+        const url = body.results[randomBook].amazon_product_url;
+        fictionList = !fictionList;
+        res.render("main", { author, title, url });
+      })
+      .catch(err => {
+        console.log("Error:", err);
+        res.status(500).send("Server Error!");
+      });
+  }
+};
